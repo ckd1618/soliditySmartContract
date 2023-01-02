cebee05ec3578986a0c41f855bf57fc657e55fe2
ckd1618 <ckd1618@gmail.com>
2023-01-02 05:36:48 -0800

    updated comments

--- sc.sol
+++ sc.sol
@@ -2,12 +2,8 @@
 
 pragma solidity ^0.8.16;
 
-/*
-Create a function that takes two arguments(x and y) of type uint and returns the value for x(x + y) and 
-store this number in a state variable. 
-*/
 contract Exercise {
-    uint storedData; // State variable
+    uint storedData; 
 
     function calculation(uint x, uint y) public returns(uint) {
         storedData = x * (x + y);
